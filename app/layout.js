7b45a41b4265a9c502d5306cc66ca9b6dc28123e
Gino Stoian <ginostoian@gmail.com>
2025-05-11 12:57:26 +0300

    add shadcn and componets

--- app/layout.js
+++ app/layout.js
@@ -3,14 +3,16 @@ import { getSEOTags } from "@/libs/seo";
 import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
 import "./globals.css";
+import { HeroHeader } from "@/components/hero5-header";
+import FooterSection from "@/components/footer.jsx";
 
 const font = Inter({ subsets: ["latin"] });
 
 export const viewport = {
-	// Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
-	themeColor: config.colors.main,
-	width: "device-width",
-	initialScale: 1,
+  // Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
+  themeColor: config.colors.main,
+  width: "device-width",
+  initialScale: 1,
 };
 
 // This adds default SEO tags to all pages in our app.
@@ -18,16 +20,18 @@ export const viewport = {
 export const metadata = getSEOTags();
 
 export default function RootLayout({ children }) {
-	return (
-		<html
-			lang="en"
-			data-theme={config.colors.theme}
-			className={font.className}
-		>
-			<body>
-				{/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
-				<ClientLayout>{children}</ClientLayout>
-			</body>
-		</html>
-	);
+  return (
+    <html
+      lang="en"
+      data-theme={config.colors.theme}
+      className={font.className}
+    >
+      <body>
+        {/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
+        <HeroHeader />
+        <ClientLayout>{children}</ClientLayout>
+        <FooterSection />
+      </body>
+    </html>
+  );
 }
