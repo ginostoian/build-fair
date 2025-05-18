7e0676b2e83f100d2b23bdc636bb62aecad91c03
Gino Stoian <ginostoian@gmail.com>
2025-05-18 14:33:54 +0300

    change text for link

--- components/hero-section.jsx
+++ components/hero-section.jsx
@@ -84,7 +84,7 @@ export default function HeroSection() {
                     className="hover:bg-background dark:hover:border-t-border bg-muted group mx-auto flex w-fit items-center gap-4 rounded-full border p-1 pl-4 shadow-md shadow-zinc-950/5 transition-colors duration-300 dark:border-t-white/5 dark:shadow-zinc-950"
                   >
                     <span className="text-foreground text-sm">
-                      Introducing Support for AI Models
+                      Introducing BuildFair for homeowners
                     </span>
                     <span className="dark:border-background block h-4 w-0.5 border-l bg-white dark:bg-zinc-700"></span>
 
